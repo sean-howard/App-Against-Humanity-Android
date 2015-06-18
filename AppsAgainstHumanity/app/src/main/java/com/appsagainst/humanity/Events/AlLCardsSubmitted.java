8608e6f7dc6d77d0e95b4a2475faac843cbd5ba2
ChrisOwen101 <cowen1992@hotmail.co.uk>
2015-06-18 18:00:06 +0100

    Missed some files

---
+++ AppsAgainstHumanity/app/src/main/java/com/appsagainst/humanity/Events/AlLCardsSubmitted.java
@@ -0,0 +1,11 @@
+package com.appsagainst.humanity.Events;
+
+/**
+ * Created by Chris on 18/06/2015.
+ */
+public class AllCardsSubmitted {
+
+    public AllCardsSubmitted(){
+
+    }
+}
--- AppsAgainstHumanity/app/src/main/java/com/appsagainst/humanity/POJO/Data.java
+++ AppsAgainstHumanity/app/src/main/java/com/appsagainst/humanity/POJO/Data.java
@@ -11,10 +11,7 @@ public class Data implements Serializable{
 
     public String playerName;
     public String uniqueID;
-    public ArrayList<Integer> whiteCardIDs;
     public String blackCardPlayerUniqueID;
-    public int winnerUniqueID = -1;
-    public int blackCardID = -1;
     public int whiteCardID = -1;
     public HashMap<String, ArrayList<Integer>> initialCards;
 }
